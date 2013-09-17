4d0e81c2093edc710c5891448f30d65e245e25f7
kevinbayes <kevin@bayes.me>
2013-09-17 11:31:32 +0200

    Add unit test for PingEndpoint

--- src/test/java/me/bayes/vertx/vest/endpoint/TestPingEndpoint.java
+++ src/test/java/me/bayes/vertx/vest/endpoint/TestPingEndpoint.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.*;
 import me.bayes.vertx.vest.deploy.VestVerticle;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.HttpClient;
+import org.vertx.java.core.http.HttpClientRequest;
+import org.vertx.java.core.http.HttpClientResponse;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.testtools.TestVerticle;
 import org.vertx.testtools.VertxAssert;
 
 public class TestPingEndpoint extends TestVerticle {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TestPingEndpoint.class);
 	
 	private static final String JSON_CONFIG_CLASSES_TO_ADD = "{\"vestClasses\":[\"me.bayes.vertx.vest.endpoint.PingEndpoint\"]}";
 
@@ -18,9 +27,31 @@ public class TestPingEndpoint extends TestVerticle {
 		JsonObject config = new JsonObject(JSON_CONFIG_CLASSES_TO_ADD);
 
 		container.deployVerticle("me.bayes.vertx.vest.deploy.VestVerticle", config);
-			
-		VertxAssert.testComplete();
 		
+		HttpClient client = vertx.createHttpClient();
+		client.setHost("localhost");
+		client.setPort(8080);
+		
+		HttpClientRequest request = client.get("/ping/", new Handler<HttpClientResponse>() {
+		    public void handle(HttpClientResponse resp) {
+		    	resp.bodyHandler(new Handler<Buffer>() {
+		            public void handle(Buffer body) {
+		               
+		               final String payload = body.toString();
+		               
+		               VertxAssert.testComplete();
+		               
+		               assertTrue(payload.equals("ping"));
+		               
+		            }
+		        }); 
+		    	
+		    	
+		    }
+		});
+		
+		request.end();
+			
 	}
 
 }
